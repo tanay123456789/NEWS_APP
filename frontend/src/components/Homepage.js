15f6cabe001b62f00c87e83895af2b24be5e6051
Tanay <tanaydwivedi456@gmail.com>
2021-11-17 10:41:29 +0530

    fix:added API_KEY in parenthesis

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -2,15 +2,18 @@ import React from "react";
 
 const Homepage=()=>{
     let API_KEY=process.env.REACT_APP_API_KEYS;
-    let url = `https://newsapi.org/v2/everything?q=Apple&from=2021-11-16&sortBy=popularity&apiKey=API_KEY`;
-    let req=new Request(url);
+    let url = `https://newsapi.org/v2/everything?q=Apple&from=2021-11-16&sortBy=popularity&apiKey=${API_KEY}`;
+    /*let req=new Request(url);*/
 
  function fetchData(){
-    fetch(req)
+    fetch(url)
       .then(function(res){
+        
         console.log(res.json())
+      
 
       })
+      
      }
       
    
