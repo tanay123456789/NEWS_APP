61b5b2a7bc3ee71e664ce3620b8b57f9e2c2a776
Tanay <tanaydwivedi456@gmail.com>
2022-03-24 10:24:05 +0530

    configured eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
